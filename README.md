63fb9e37c626919379cc69343e1e49c7865cbe7e
windsor718 <winzer718@gmail.com>
2020-02-17 14:00:33 -0500

    remove tracked caches

--- README.md
+++
@@ -1,2 +0,0 @@
-# pyHRR
-A simple python wrapper to handle the HRR model. DAssim.py is a sample code to conduct data assimilation coupled with the pyHRR.   
--- batch_cal03_syears.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ~/.bashrc
-cd $LSB_JOB_CWD
-
-/home/yi79a/anaconda3/bin/python /home/yi79a/DA/missouri/pyHRR/dAssim_beck_cal03_syears.py
